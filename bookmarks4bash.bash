f6480aa3e0be7878519750d4af27c158e68de263
Flavio Barbara <fb@maidaccordo.net>
2017-11-24 04:28:44 +0100

    - new file format (not backward compatible) - added space management both in bookmark names and in paths - fixed a bug in deleting bookmark from path

--- bookmarks4bash.bash
+++ bookmarks4bash.bash
@@ -60,12 +60,12 @@ __bb_load_bookmarks(){
     local value
     exec 4<$1
     set -f ## disable pathname expansion
-    while IFS=' ' read -u 4 -r key value || [[ -n "${key}" ]] ; do
-	[[ ${key} =~ ^[[:blank:]]*$ ]] && continue        # skip blank lines
-	[[ ${key} =~ ^[[:blank:]]*#.* ]] && continue  # skip comment lines
-	[[ "${value}" =~ ^[[:blank:]]*#.* ]] && continue
-	[[ -z ${value} ]] && continue # skip empty bookmarks
-	_Bstore[$key]=${value}
+    while IFS='|' read -u 4 -r key value || [[ -n "${key}" ]] ; do
+        [[ "${key}" =~ ^[[:blank:]]*$ ]] && continue        # skip blank lines
+        [[ "${key}" =~ ^[[:blank:]]*#.* ]] && continue  # skip comment lines
+        [[ "${value}" =~ ^[[:blank:]]*#.* ]] && continue
+        [[ -z "${value}" ]] && continue # skip empty bookmarks
+        _Bstore[$key]="${value}"
     done
     set +f
     exec 4>&-
@@ -82,19 +82,19 @@ __bb_reload_bmarks(){
 __bb_load_bookmarks ${BBMARKSFILE}
 
 __bb_search_key(){
-    local key=$1
+    local key="$1"
     local k
-    for k in ${!_Bstore[@]} ; do
-	[[ ${k} == ${key} ]] && return 0
+    for k in "${!_Bstore[@]}" ; do
+        [[ "${k}" == "${key}" ]] && return 0
     done
     return 1
 }
 
 __bb_search_val(){
-    local val=$1
+    local val="$1"
     local k
-    for k in ${!_Bstore[@]} ; do
-	[[ ${!_Bstore[$k]} == ${val} ]] && __bb_reply=$k && return 0
+    for k in "${!_Bstore[@]}" ; do
+        [[ "${_Bstore[$k]}" == "${val}" ]] && __bb_reply="$k" && return 0
     done
     return 1
 }
@@ -107,41 +107,41 @@ __bb_ask4overwrite(){
 }
 
 __bb_writefile(){
-    local fle=$1
+    local fle="$1"
     : > ${fle:=${BBMARKSFILE}}
-    for k in ${!_Bstore[@]} ; do
-	echo ${k} ${_Bstore[$k]} >> ${fle}
+    for k in "${!_Bstore[@]}" ; do
+        echo "${k}|${_Bstore[$k]}" >> "${fle}"
     done
 }
 
 __bb_list(){
     local bm ll strmax=0
-    for bm in ${ll:=${!_Bstore[@]}} ; do
-	(( strmax < ${#bm} )) && strmax=${#bm}
+    [[ "$1" ]] && ll=( "$@" ) || ll=( "${!_Bstore[@]}" )
+    for bm in "${ll[@]}" ; do
+        (( strmax < ${#bm} )) && strmax=${#bm}
     done
     (( strmax+=2 ))
-    [[ $1 ]] && ll=$@
-    for bm in ${ll} ; do
-	printf " %-${strmax}s%s\n" $bm ${_Bstore[$bm]}
+    for bm in "${ll[@]}" ; do
+        printf " %-${strmax}s%s\n" "${bm}" "${_Bstore[$bm]}"
     done
 }
 
 __bb_add(){
-    local bookmark=$1
-    local path=$2
-    [[ -d ${path} ]] && [[ -x ${path} ]] ||
-	{ __to_stderr "Path of destination doesn't exist or is not reachable, bookmark not created" ; return 1 ; }
-    pushd ${path} > /dev/null
-    path=${PWD}
+    local bookmark="$1"
+    local path="$2"
+    [[ -d "${path}" ]] && [[ -x "${path}" ]] ||
+        { __to_stderr "Path of destination doesn't exist or is not reachable, bookmark not created" ; return 1 ; }
+    pushd "${path}" > /dev/null
+    path="${PWD}"
     popd > /dev/null
-    __bb_search_key ${bookmark} && 
-	! __bb_ask4overwrite || { _Bstore[$bookmark]=${path} && __bb_writefile ; }
+    __bb_search_key "${bookmark}" && 
+        ! __bb_ask4overwrite || { _Bstore[$bookmark]="${path}" && __bb_writefile ; }
 }
 
 __bb_del(){
-    local bookmark=$1
-    __bb_search_key ${bookmark} && { unset _Bstore[$bookmark] && __bb_writefile ; } ||
-	{ __to_stderr "Bookmark does not exist" ; return 1 ; }
+    local bookmark="$1"
+    __bb_search_key "${bookmark}" && { unset "_Bstore[${bookmark}]" && __bb_writefile ; } ||
+        { __to_stderr "Bookmark does not exist" ; return 1 ; }
 }
 
 __bb_reset(){
@@ -152,27 +152,31 @@ __bb_reset(){
 }
 
 __bb_del_path(){
-    local path=$1 tmp
-    __bb_search_val ${path}
+    local path="$1" tmp
+    pushd "${path}" > /dev/null
+    path="${PWD}"
+    popd > /dev/null
+    __bb_search_val "${path}"
     tmp=$?
     if (( tmp )) ; then
-	__to_stderr "${path} not found in bookmarks"
-	return $tmp
+        __to_stderr "${path} not found in bookmarks"
+        return $tmp
     else
-	echo "Deleting bookmark"
-	__bb_list __bb_reply
-	__bb_del __bb_reply
-	__bb_reply=
+        echo "Deleting bookmark"
+        __bb_list "${__bb_reply}"
+        __bb_del "${__bb_reply}"
+        __bb_reply=
     fi
 }
 
 __bb_goto(){
-    local bm  bmark=$1
-    __bb_search_key ${bmark} || return 1
-    [[ -d ${_Bstore[$bmark]} ]] || return 2
-    [[ -x ${_Bstore[$bmark]} ]] || return 3
-    for bm in ${!_Bstore[@]} ; do
-	[[ ${bmark} == ${bm} ]] && cd ${_Bstore[$bm]} && return 0
+    local bm  bmark="$1"
+    __bb_search_key "${bmark}" || return 1
+    [[ -d "${_Bstore[$bmark]}" ]] || return 2
+    [[ -x "${_Bstore[$bmark]}" ]] || return 3
+    for bm in "${!_Bstore[@]}" ; do
+	echo "$bm | $bmark"
+        [[ "${bmark}" == "${bm}" ]] && cd "${_Bstore[$bm]}" && return 0
     done
     return 1
 }
@@ -181,11 +185,11 @@ valid_optarg(){
     ## invalid OPTARG if it begins with a dash
     # (( ${#OPTARG} > 1 )) && [[ ${OPTARG:0:1} == '-' ]] && {
     [[ ${OPTARG:0:1} == '-' ]] && {
-	__to_stderr "Argument of -${opt} option cannot begin with -" 
-	OPTARG=$opt
-	((OPTIND-=1))
-	opt=":"
-	return 1
+        __to_stderr "Argument of -${opt} option cannot begin with -" 
+        OPTARG=$opt
+        ((OPTIND-=1))
+        opt=":"
+        return 1
     }
     return 0
 }
@@ -197,77 +201,77 @@ bb(){
     local i nexpos toadd todel tmp sum opt
     while getopts ':adhlr-' opt ; do
     # while getopts ':a:bcd:-' opt ; do
-	i=1
-	while (( i )) ; do
-	    i=0
-	    case $opt in
-		# interpreting double dash as the end of the options
-		# a) valid_optarg && {
-		# 	 echo argomento valido  $opt ${OPTIND} ${OPTARG} ; } || {
-		# 	 echo argomento non valido $opt $OPTIND $OPTARG ; i=1 ; continue ; }
-		#  ;;
-		# b is an option with an optional argument
-		# b) zz=${@:$OPTIND:1} ;
-		#    [[ ${zz:0:1} != '-' ]] && {
-		#        OPTARG=$zz
-		#        ((OPTIND+=1))
-		#    }
-		#    echo $opt $OPTIND $OPTARG
-		#    ;;
-		# c) echo $opt $OPTIND $OPTARG
-		#    ;;
-		# d) echo $opt $OPTIND $OPTARG
-		#    ;;
-		# -) break 2
-		#    ;;
-		# \:) echo opzione -${OPTARG} necessita di argomento obligatorio
-		#     return 1
-		#     ;;
-		# \?) echo opzione -${OPTARG} inesistente && return 1 ;
-		
-		h) __bb_printUsage
-		   return $?
-		   ;;
-		l) llist=1
-		   ;;
-		a) ladd=1
-		   nexpos=${@:$OPTIND:1}
-		   [[ ${nexpos:0:1} != '-' ]] && {
-		       toadd=${nexpos}
-		       (( OPTIND+=1 ))
-		   }
-		   ;;
-		d) ldel=1
-		   nexpos=${@:$OPTIND:1}
-		   [[ ${nexpos:0:1} != '-' ]] && {
-		       todel=${nexpos}
-		       (( OPTIND+=1 ))
-		   }
-		   ;;
-		r) lreset=1
-		   ;;
-		# f) valid_optarg && {
-		# 	 laltfile=1
-		# 	 newBfile=${OPTARG}
-		#      } || { i=1 ; continue ; }
-		#    ;;
-		-) break 2
-		   ;;
-		\:) __to_stderr "Option -${OPTARG} needs a mandatory (valid) argument"
-		    return 1
-		    ;;
-		\?) __to_stderr "Option -${OPTARG} doesn't exist" && return 1
-		    ;;
-	    esac
-	done
+        i=1
+        while (( i )) ; do
+            i=0
+            case $opt in
+                # interpreting double dash as the end of the options
+                # a) valid_optarg && {
+                #        echo argomento valido  $opt ${OPTIND} ${OPTARG} ; } || {
+                #        echo argomento non valido $opt $OPTIND $OPTARG ; i=1 ; continue ; }
+                #  ;;
+                # b is an option with an optional argument
+                # b) zz=${@:$OPTIND:1} ;
+                #    [[ ${zz:0:1} != '-' ]] && {
+                #        OPTARG=$zz
+                #        ((OPTIND+=1))
+                #    }
+                #    echo $opt $OPTIND $OPTARG
+                #    ;;
+                # c) echo $opt $OPTIND $OPTARG
+                #    ;;
+                # d) echo $opt $OPTIND $OPTARG
+                #    ;;
+                # -) break 2
+                #    ;;
+                # \:) echo opzione -${OPTARG} necessita di argomento obligatorio
+                #     return 1
+                #     ;;
+                # \?) echo opzione -${OPTARG} inesistente && return 1 ;
+                
+                h) __bb_printUsage
+                   return $?
+                   ;;
+                l) llist=1
+                   ;;
+                a) ladd=1
+                   nexpos=${@:$OPTIND:1}
+                   [[ ${nexpos:0:1} != '-' ]] && {
+                       toadd="${nexpos}"
+                       (( OPTIND+=1 ))
+                   }
+                   ;;
+                d) ldel=1
+                   nexpos=${@:$OPTIND:1}
+                   [[ ${nexpos:0:1} != '-' ]] && {
+                       todel="${nexpos}"
+                       (( OPTIND+=1 ))
+                   }
+                   ;;
+                r) lreset=1
+                   ;;
+                # f) valid_optarg && {
+                #        laltfile=1
+                #        newBfile=${OPTARG}
+                #      } || { i=1 ; continue ; }
+                #    ;;
+                -) break 2
+                   ;;
+                \:) __to_stderr "Option -${OPTARG} needs a mandatory (valid) argument"
+                    return 1
+                    ;;
+                \?) __to_stderr "Option -${OPTARG} doesn't exist" && return 1
+                    ;;
+            esac
+        done
     done
     sum=$(( llist+ladd+ldel+lreset ))
 
     (( sum+${#@} )) || { __bb_printUsage || return 1 ; }
     
     if (( sum > 1 )) ; then
-	__to_stderr "Options -a, -d, -l and -r are mutually exclusive. Only one of these can be used at once"
-	return 2
+        __to_stderr "Options -a, -d, -l and -r are mutually exclusive. Only one of these can be used at once"
+        return 2
     fi
     
     shift $(($OPTIND - 1))
@@ -279,56 +283,56 @@ bb(){
     (( lreset )) && __bb_reset && return 0
 
     (( ${#@} > 1 )) &&
-	__to_stderr "Only one positional argument allowed, using the first one"
+        __to_stderr "Only one positional argument allowed, using the first one"
     
-    nexpos=$1
+    nexpos="$1"
 
-    toadd=${toadd:-'-'}
-    todel=${todel:-'-'}
+    toadd="${toadd:--}"
+    todel="${todel:--}"
     
     if (( ladd )) ; then
-	# __bb_add $bookmark $path
-	if [[ ${toadd} == '-' ]] ; then
-	    # here if -a without parameter
-	    nexpos=${nexpos:-${PWD}}
-	    [[ ${nexpos} =~ (.*)/$ ]] && nexpos=${BASH_REMATCH[1]}
-	    __bb_add ${nexpos##*/} ${nexpos} || return $?
-	    # if [[ -z ${nexpos} ]] ; then
-	    # 	# without $1
-	    # 	__bb_add ${PWD##*/} ${PWD} || return $?
-	    # else
-	    # 	__bb_add ${nexpos##*/} ${nexpos} || return $?
-	    # fi
-	else
-	    # here if -a with parameter
-	    nexpos=${nexpos:-${PWD}}
-	    __bb_add ${toadd} ${nexpos} || return $?
-	    # if [[ -z ${nexpos} ]] ; then
-	    # 	# without $1
-	    # 	__bb_add ${toadd} ${PWD} || return $?
-	    # else
-	    # 	__bb_add ${toadd} ${nexpos} || return $?
-	    # fi
-	fi
+        # __bb_add $bookmark $path
+        if [[ "${toadd}" == '-' ]] ; then
+            # here if -a without parameter
+            nexpos="${nexpos:-${PWD}}"
+            [[ "${nexpos}" =~ (.*)/$ ]] && nexpos="${BASH_REMATCH[1]}"
+            __bb_add "${nexpos##*/}" "${nexpos}" || return $?
+            # if [[ -z ${nexpos} ]] ; then
+            #   # without $1
+            #   __bb_add ${PWD##*/} ${PWD} || return $?
+            # else
+            #   __bb_add ${nexpos##*/} ${nexpos} || return $?
+            # fi
+        else
+            # here if -a with parameter
+            nexpos="${nexpos:-${PWD}}"
+            __bb_add "${toadd}" "${nexpos}" || return $?
+            # if [[ -z ${nexpos} ]] ; then
+            #   # without $1
+            #   __bb_add ${toadd} ${PWD} || return $?
+            # else
+            #   __bb_add ${toadd} ${nexpos} || return $?
+            # fi
+        fi
     elif (( ldel )) ; then
-	if [[ ${todel} == '-' ]] ; then
-	    # here if -d without parameter
-	    nexpos=${nexpos:-${PWD}}
-	    __bb_del_path ${nexpos} || return $?
-	else
-	    # here if -d with parameter
-	    if [[ -z ${nexpos} ]] ; then
-		__bb_del ${todel} || return $?
-	    else
-		[[ ${nexpos} =~ (.*)/$ ]] && nexpos=${BASH_REMATCH[1]}
-		[[ ${_Bstore[$todel]} == ${nexpos} ]] && __bb_del ${todel} || {
-			__to_stderr "There is no bookmark ${todel} pointing to ${nexpos}"
-			return 1
-		    }
-	    fi
-	fi
+        if [[ "${todel}" == '-' ]] ; then
+            # here if -d without parameter
+            nexpos="${nexpos:-${PWD}}"
+            __bb_del_path "${nexpos}" || return $?
+        else
+            # here if -d with parameter
+            if [[ -z "${nexpos}" ]] ; then
+                __bb_del "${todel}" || return $?
+            else
+                [[ "${nexpos}" =~ (.*)/$ ]] && nexpos="${BASH_REMATCH[1]}"
+                [[ "${_Bstore[$todel]}" == "${nexpos}" ]] && __bb_del "${todel}" || {
+                        __to_stderr "There is no bookmark ${todel} pointing to ${nexpos}"
+                        return 1
+                    }
+            fi
+        fi
     else
-	__bb_goto ${nexpos} || {
+	__bb_goto "${nexpos}" || {
 	    __to_stderr ' '"${nexpos}  ${_Bstore[nextpos]}\n Bookmark or destination doesn't exist" 
 	    return 1
 	}
